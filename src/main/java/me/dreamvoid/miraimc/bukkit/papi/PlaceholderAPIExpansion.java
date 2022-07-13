5dffd5ae44dd7313bf4797175878c0c7fd85ece4
流光溢彩Last <dreamvoid@dreamvoid.ml>
2022-07-13 23:54:19 +0800

    1.2

--- src/main/java/me/dreamvoid/miraimc/bukkit/papi/PlaceholderAPIExpansion.java
+++ src/main/java/me/dreamvoid/miraimc/bukkit/papi/PlaceholderAPIExpansion.java
@@ -12,8 +12,22 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
+import java.util.List;
 
 public class PlaceholderAPIExpansion extends PlaceholderExpansion {
+    // For development.
+    private final boolean enforcePlugin = false;
+    private final Plugin plugin = Bukkit.getPluginManager().getPlugin("MiraiMC");
+    private final boolean hasPlugin;
+
+    public PlaceholderAPIExpansion(){
+        hasPlugin = plugin != null && plugin.isEnabled();
+    }
+
+    @Override
+    public String getName() {
+        return "MiraiMC";
+    }
 
     @Override
     public String getIdentifier() {
@@ -27,80 +41,77 @@ public class PlaceholderAPIExpansion extends PlaceholderExpansion {
 
     @Override
     public String getVersion() {
-        return "1.1.1";
+        return "1.2";
     }
 
     @Override
     public String getRequiredPlugin() {
-        return "MiraiMC";
+        return enforcePlugin ? "MiraiMC" : null;  // I need to monitoring plugin status.
     }
 
     @Override
     public boolean canRegister() {
-        Plugin plugin = Bukkit.getPluginManager().getPlugin(getRequiredPlugin());
-        return plugin != null && plugin.isEnabled();
+        return !enforcePlugin || (plugin != null && plugin.isEnabled()); // Let "isworking" working.
     }
 
     @Override
     public String onRequest(OfflinePlayer p, String params) {
         String[] args = params.split("_");
         switch (args[0].toLowerCase()) {
-            case "isonline":{
-                if(args.length>=2) {
+            case "isworking":{
+                return String.valueOf(hasPlugin);
+            }
+            case "version":
+                if(hasPlugin) return plugin.getDescription().getVersion();
+                break;
+            case "isonline":
+                if(hasPlugin && args.length>=2) {
                     MiraiBot miraiBot = MiraiBot.getBot(Long.parseLong(args[1]));
                     if (miraiBot.isExist() && miraiBot.isOnline()) {
                         return "true";
                     } else return "false";
                 }
                 break;
-            }
-            case "counts":{
-                return String.valueOf(MiraiBot.getOnlineBots().toArray().length);
-            }
-            case "nick":{
-                if(args.length>=2){
-                    MiraiBot miraiBot = MiraiBot.getBot(Long.parseLong(args[1]));
-                    if (miraiBot.isExist() && miraiBot.isOnline()) {
-                        return miraiBot.getNick();
+            case "counts":
+                if(hasPlugin) return String.valueOf(MiraiBot.getOnlineBots().toArray().length);
+                break;
+            case "nick":
+                if(hasPlugin && args.length>=2){
+                    MiraiBot bot = MiraiBot.getBot(Long.parseLong(args[1]));
+                    if (bot.isExist() && bot.isOnline()) {
+                        return bot.getNick();
                     }
                 }
                 break;
-            }
-            case "friendcounts":{
-                if(args.length>=2) {
+            case "friendcounts":
+                if(hasPlugin && args.length>=2) {
                     MiraiBot miraiBot = MiraiBot.getBot(Long.parseLong(args[1]));
                     if (miraiBot.isExist() && miraiBot.isOnline()) {
                         return String.valueOf(miraiBot.getFriendList().toArray().length);
                     }
                 }
                 break;
-            }
-            case "groupcounts":{
-                MiraiBot miraiBot = MiraiBot.getBot(Long.parseLong(args[1]));
-                if (miraiBot.isExist() && miraiBot.isOnline()) {
-                    return String.valueOf(miraiBot.getGroupList().toArray().length);
+            case "groupcounts":
+                if(hasPlugin) {
+                    MiraiBot miraiBot = MiraiBot.getBot(Long.parseLong(args[1]));
+                    if (miraiBot.isExist() && miraiBot.isOnline()) {
+                        return String.valueOf(miraiBot.getGroupList().toArray().length);
+                    }
                 }
                 break;
-            }
-            case "bindqq": {
-                if (args.length >= 2) {
+            case "bindqq":
+                if (hasPlugin && args.length >= 2) {
                     OfflinePlayer player = Bukkit.getPlayer(args[1]);
                     return String.valueOf(MiraiMC.getBind(player.getUniqueId()));
                 } else return String.valueOf(MiraiMC.getBind(p.getUniqueId()));
-            }
-            case "binduuid":{
-                if(args.length >= 2) {
-                    return MiraiMC.getBind(Long.parseLong(args[1])).toString();
-                }
+            case "binduuid":
+                if(hasPlugin && args.length >= 2) return MiraiMC.getBind(Long.parseLong(args[1])).toString();
                 break;
-            }
-            case "bindname":{
-                if(args.length >= 2) {
-                    return Bukkit.getOfflinePlayer(MiraiMC.getBind(Long.parseLong(args[1]))).getName();
-                }
+            case "bindname":
+                if(hasPlugin && args.length >= 2) return Bukkit.getOfflinePlayer(MiraiMC.getBind(Long.parseLong(args[1]))).getName();
                 break;
-            }
-            default:return null;
+            default:
+                return null;
         }
         return null;
     }
